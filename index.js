551f4065006d363a9b92081a08e73ec43bcba0b9
Shaunak G <shaunakg@github.com>
2019-12-03 21:02:41 +1100

    ALmost done., just need to finish handling shortlinks

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var http = require('http').createServer(app);
 var url = require('url');
 var querystring = require('querystring');
 var request = require('request');
+var htmlparser = require('parse-entities');
 
 const PORT = process.env.PORT || 8080;
 
@@ -13,9 +14,13 @@ app.get('/', function (req, res) {
     res.sendFile('static/index.html');
 })
 
+app.get('/reading_stylesheet.css', function (req,res) {
+    res.sendFile("static/reading_stylesheet.css");
+})
+
 app.get('/simplify.html', function (req, res) {
 
-    console.log("Request to simplify.html");
+    console.log("Simplifying page was visted");
 
     let fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
     let parsedUrl = url.parse(fullUrl);
@@ -23,11 +28,36 @@ app.get('/simplify.html', function (req, res) {
 
     if ( Object.keys(qsjson).length == 1 && qsjson.url.includes('redd') ) {
 
-        let redditurl = qsjson.url + ".json";
-        res.send(redditurl);
+        var redditurl = qsjson.url.replace("http:", "https:");
+
+        if (redditurl.includes("redd.it")) {
+            console.log("This is a reddit shortlink URL");
+            request(qsjson.url, function (error, response, body) {
+                if (response.statusCode > 299 && response.statusCode < 399) {
+                    console.log("Grabbed actual location headers");
+                    redditurl = response.headers['location'] + ".json";
+                } else {
+                    redditurl = qsjson.url.replace("http:", "https:") + ".json";
+                }
+            })
+        } else {
+            redditurl = qsjson.url.replace("http:", "https:") + ".json";
+        }
+
+        console.log("Starting request to: " + redditurl);
 
         request(redditurl, function (error, response, body) {
             
+            var post_obj = JSON.parse(body);
+            var post_text_html = htmlparser(post_obj[0].data.children[0].data.selftext_html).replace(/<a href="/g,'<a href="https://redditforkindle.herokuapp.com/simplify.html?url=');
+            var post_title = htmlparser(post_obj[0].data.children[0].data.title);
+
+            res.write("<!doctype html><head><link rel='stylesheet' href='reading_stylesheet.css'><title>" + post_title + " (" + req.headers['user-agent'] + ")</title></head>")
+            res.write("<h1>"+post_title+"</h1>");
+            res.write(post_text_html);
+            res.write("<div class='footer'>Created by <a href='https://redditforkindle.herokuapp.com'>redditforkindle.herokuapp.com</a></div>")
+            res.end();
+
         });
 
     } else {
